8e27b11a43af497ad1c7972727a766f842a59e04
Nazrat450 <145197951+Nazrat450@users.noreply.github.com>
2024-10-10 22:48:15 +1100

    first commit

---
+++ housekeeping/src/pages/About.tsx
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+
+const About: React.FC = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>This is the about page. Here you can add more information about your website.</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ housekeeping/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to My React App!</h1>
+            <p>This is the home page.</p>
+        </div>
+    );
+};
+
+export default Home;
